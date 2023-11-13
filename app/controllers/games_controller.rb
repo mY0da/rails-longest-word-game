61ad12228d775ad26d529a0e1d095a25b86ae1e3
Gustavo Camargo <gustavoscamargo@outlook.com>
2023-11-13 16:52:54 +0000

    Done

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,34 @@
+require 'open-uri'
+require 'json'
+require 'date'
+
+class GamesController < ApplicationController
+  def new
+    @time = Time.now
+    w = ("A".."Z").to_a
+    @letters = (w + w).sample(10)
+  end
+
+  def score
+    @letters = params[:letters].split
+    grid_r = @letters.size
+    @old_time = params[:time].to_time
+    @h = { your_score: 0, message: "not an english word", time: Time.now - @old_time}
+    params[:word].upcase.chars.each { |letter| @letters.delete_at(@letters.index(letter)) if @letters.include?(letter) }
+    if english?(params[:word]) && params[:word].chars.size + @letters.size == grid_r
+      @h[:score] = params[:word].length / @h[:time]
+      @h[:message] = "Well done"
+    elsif params[:word].chars.size + @letters.size != grid_r
+      @h[:message] = "not in the grid"
+    end
+  end
+
+  private
+
+  def english?(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    user_serialized = URI.open(url).read
+    user = JSON.parse(user_serialized)
+    return user["found"]
+  end
+end
